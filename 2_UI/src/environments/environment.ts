da2627d1f4df671dfcd7c8ba557af2db0a14c14f
Vraj Mistry <mistryvraj3198@gmail.com>
2021-04-18 20:09:44 -0700

    Project on multi-region
    Project Big data

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'eu-west-1:0b9e9536-4ee8-47fd-bd1b-12427a989573',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '1745285752309844',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.crv228.de/'
 
 };
